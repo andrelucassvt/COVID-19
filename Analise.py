8722ccdc9d3017ba2b5e33100a269e8b1b07359c
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-23 11:01:55 -0300

    Grafico atualizado constantemente

--- Analise.py
+++ Analise.py
@@ -2,8 +2,15 @@
 
 import matplotlib.pyplot as plt
 
-x = ["18","19","20","21","22","23"]
-y = ["1","1","2","2","4","4"]
+##Belém
+x = ["17","18","19","20","21","22","23"]
+y = ["0","1","1","2","2","4","4"]
+tamanhoB = [0,50,0,50,0,50,50]
+
+##Marabá
+x2 = ["17","18","19","20","21","22","23"]
+y2 =["0","0","0","0","0","0","1"]
+tamanhoM = [0,0,0,0,0,0,50]
 
 ##Legendas do grafico
 titulo ="Casos confirmado do COVID-19 PA"
@@ -12,10 +19,13 @@ laby = "Quantidade de infectados"
 
 ##Processamento
 plt.title(titulo)
-##Criando grafico de linha
+##Criando grafico de linha de Belém
 plt.plot(x,y, label = "Belém")
+##Criando grafico de linha Marabá
+plt.plot(x2,y2, label = "Marabá")
 ##Pontos no grafico
-plt.scatter(x,y, color = "r")
+plt.scatter(x,y, color = "r", s= tamanhoB)
+plt.scatter(x2,y2, s= tamanhoM)
 
 ##Legendando de fato o grafico
 plt.xlabel(Labx)
