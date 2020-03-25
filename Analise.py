e94cc1355e70779f6a5cac57497f06025b9fda26
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-25 14:23:52 -0300

    Atualizao do grafico, novos casos adicionados

--- Analise.py
+++ Analise.py
@@ -3,15 +3,19 @@
 import matplotlib.pyplot as plt
 
 ##Belém
-x = ["17","18","19","20","21","22","23"]
-y = ["0","1","1","2","2","4","4"]
-tamanhoB = [0,50,0,50,0,50,50]
+x = ["17","18","19","20","21","22","23","24","25"]
+y = ["0","1","1","2","2","4","4","4","4"]
+tamanhoB = [0,50,0,50,0,50,0,0,50]
 
 ##Marabá
-x2 = ["17","18","19","20","21","22","23"]
-y2 =["0","0","0","0","0","0","1"]
-tamanhoM = [0,0,0,0,0,0,50]
+x2 = ["17","18","19","20","21","22","23","24","25"]
+y2 =["0","0","0","0","0","0","1","1","1"]
+tamanhoM = [0,0,0,0,0,0,50,0,50]
 
+##Ananindeua
+x3 = ["17","18","19","20","21","22","23","24","25"]
+y3 = ["0","0","0","0","0","0","0","0","2"]
+tamanhoA = [0,0,0,0,0,0,0,0,50]
 ##Legendas do grafico
 titulo ="Casos confirmado do COVID-19 PA"
 Labx = "Dias de Março"
@@ -23,10 +27,12 @@ plt.title(titulo)
 plt.plot(x,y, label = "Belém")
 ##Criando grafico de linha Marabá
 plt.plot(x2,y2, label = "Marabá")
+##Criando grafico de linha Ananindeua
+plt.plot(x3,y3, label = "Ananindeua")
 ##Pontos no grafico
 plt.scatter(x,y, color = "r", s= tamanhoB)
 plt.scatter(x2,y2, s= tamanhoM)
-
+plt.scatter(x3,y3, s= tamanhoA)
 ##Legendando de fato o grafico
 plt.xlabel(Labx)
 plt.ylabel(laby)
