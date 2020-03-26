4972fade8b80788ff65c5bf9bf080307cdb6bc1f
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-26 13:09:00 -0300

    Atualização dos dados

--- Grafico_de_Barras.py
+++ Grafico_de_Barras.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 
 ##VAlores
-cidades = ['0','Itaituba','Marabá','Castanhal','Ananindeua','Belém']
+cidades = ['0','Itaituba','Marabá','Castanhal','Não divulgado','Ananindeua','Belém']
 
-casos = ['0','1','1','1','4','4']
+casos = ['0','1','1','1','2','2','6']
 
 titulo = "Casos confirmados de COVID-19 no Estado do PA (Casos: 13)"
 cidadesx = "Quantidade de infectados"
