63b62ca4f913c2cb41fe0adeb12e47fed70d22ce
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-26 11:44:14 -0300

    Atualização mais nova

--- Analise.py
+++ Analise.py
@@ -3,21 +3,30 @@
 import matplotlib.pyplot as plt
 
 ##Belém
-x = ["17","18","19","20","21","22","23","24","25"]
-y = ["0","1","1","2","2","4","4","4","4"]
-tamanhoB = [0,50,0,50,0,50,0,0,50]
+x = ["17","18","19","20","21","22","23","24","25","26"]
+y = ["0","1","1","2","2","4","4","4","4","4"]
+tamanhoB = [0,50,0,50,0,50,0,0,50,50]
 
 ##Marabá
-x2 = ["17","18","19","20","21","22","23","24","25"]
-y2 =["0","0","0","0","0","0","1","1","1"]
-tamanhoM = [0,0,0,0,0,0,50,0,50]
+x2 = ["17","18","19","20","21","22","23","24","25","26"]
+y2 =["0","0","0","0","0","0","1","1","1","1"]
+tamanhoM = [0,0,0,0,0,0,50,0,50,50]
 
 ##Ananindeua
-x3 = ["17","18","19","20","21","22","23","24","25"]
-y3 = ["0","0","0","0","0","0","0","0","2"]
-tamanhoA = [0,0,0,0,0,0,0,0,50]
+x3 = ["17","18","19","20","21","22","23","24","25","26"]
+y3 = ["0","0","0","0","0","0","0","0","2","4"]
+tamanhoA = [0,0,0,0,0,0,0,0,50,50]
+
+##Castanhal
+x4 = ["17","18","19","20","21","22","23","24","25","26"]
+y4 =["0","0","0","0","0","0","0","0","0","1"]
+tamanhoC = [0,0,0,0,0,0,0,0,0,50]
+##Itaituba
+x5 = ["17","18","19","20","21","22","23","24","25","26"]
+y5 =["0","0","0","0","0","0","0","0","0","1"]
+tamanhoI = [0,0,0,0,0,0,0,0,0,50]
 ##Legendas do grafico
-titulo ="Casos confirmado do COVID-19 PA (CASOS: 7)"
+titulo ="Casos confirmado do COVID-19 PA (CASOS: 11)"
 Labx = "Dias de Março"
 laby = "Quantidade de infectados"
 
@@ -29,6 +38,11 @@ plt.plot(x,y, label = "Belém")
 plt.plot(x2,y2, label = "Marabá")
 ##Criando grafico de linha Ananindeua
 plt.plot(x3,y3, label = "Ananindeua")
+##Criando grafico de linha Castanhal
+plt.plot(x4,y4, label = "Castanhal")
+##Criando grafico de linha Itaituba
+plt.plot(x5,y5, label = "Itaituba")
+
 
 ##Pontos no grafico
 plt.scatter(x,y, color = "r", s= tamanhoB)
