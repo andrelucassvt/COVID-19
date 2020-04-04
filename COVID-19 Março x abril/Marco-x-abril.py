251232dedf5ce6762c207818a81b2b7161faff84
andrelucassvt <andrelucassvt99@gmail.com>
2020-04-03 22:16:51 -0300

    Organização de pastas

---
+++ COVID-19 Março x abril/Marco-x-abril.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+Meses = ["Março","Abril"]
+casos = ["32","43"]
+
+titulo = "Comparação: Março e Abril"
+EixoX = "Meses"
+EixoY = "Quantidade de casos"
+
+plt.title(titulo)
+plt.
